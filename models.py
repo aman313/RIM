903ddd62fb258f774929764e23ab49648bd34950
aman <aman313@gmail.com>
2020-01-26 02:20:33 +0530

    added comments for rimmodule implementation

--- models.py
+++ models.py
@@ -4,11 +4,21 @@ from allennlp.models import Model
 
 class RIMModule(torch.nn.Module):
 
-    def __init__(self):
-        pass
+    def __init__(self, num_kernels, active_kernels, dynamics_module):
+        raise NotImplementedError
 
-    def forward(self):
-        pass
+    def forward(self, input_sequence, prev_state, query_suffix=None):
+        '''
+
+        :param input_sequence:
+        :param prev_state:
+        :return:
+            use hidden state of each rim as query and get attention weights and attended input representation
+            select top k kernels by attention weight
+            run one step of dynamics for selected kernels
+            update by communincation with other rims
+        '''
+        raise NotImplementedError
 
 
 class RIMEncoder(Model):
